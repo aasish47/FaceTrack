b2006600670314bdb5fc1a08baa865dde9303c03
Balaram nayak <balaramnayak1133@gmail.com>
2025-02-22 22:25:28 +0530

    Added user registration form validation

--- src/app/testComponents/admin-panel/registration-form/registration-form.component.ts
+++ src/app/testComponents/admin-panel/registration-form/registration-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { Form, FormsModule, NgForm } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgModel } from '@angular/forms';
@@ -12,9 +12,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 })
 
-
-
 export class RegistrationFormComponent {
+
+  isFormSubmitted: boolean = false;
 
   //Creating user form object
 
@@ -31,25 +31,29 @@ export class RegistrationFormComponent {
 
   //Submit function
 
-  onSubmit() {
+  onSubmit(form: NgForm){
     
+    this.isFormSubmitted = true;
+    if(form.form.invalid){
+      return;
+    }
     const formValue = this.userForm;
     debugger;
-    this.onReset();
     window.alert("Registration Successful");
+    this.onReset();
+    this.isFormSubmitted = false;
   }
 
-  //Reset function
-
+   //Reset function
   onReset(){
-    this.userForm.fName= '';
-    this.userForm.lName= '';
-    this.userForm.userId= '';
-    this.userForm.gender= '';
-    this.userForm.department= '';
-    this.userForm.dob= '';
-    this.userForm.userPhoto= null;
 
-  }
-
+    this.userForm.fName = '';
+    this.userForm.lName = '';
+    this.userForm.userId = '';
+    this.userForm.gender = '';
+    this.userForm.department = '';
+    this.userForm.dob = '';
+    this.userForm.userPhoto = null;
+    this.isFormSubmitted = false;
+  }   
 }
