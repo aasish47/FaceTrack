c83f7ea73ee933bedfe42d25a2ae9cfbe3112bf8
aasish47 <aasish6001@gmail.com>
2025-03-07 10:21:15 +0530

    cleaned the main branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { AdminLoginComponent } from './components/admin-login/admin-login.component';
-import { UserLoginComponent } from './components/user-login/user-login.component';
-import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'home', component: HomeComponent},
-  { path: 'admin-login', component: AdminLoginComponent},
-  { path: 'user-login', component: UserLoginComponent},
-  { path: 'user-dashboard', component: UserDashboardComponent},
-  { path: 'admin-dashboard', component: AdminDashboardComponent}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { AdminLoginComponent } from './components/admin-login/admin-login.component';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
-import { UserLoginComponent } from './components/user-login/user-login.component';
-import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { AppComponent } from './app.component'
 import { FormsModule } from '@angular/forms';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AdminLoginComponent,
-    AdminDashboardComponent,
-    UserLoginComponent,
-    UserDashboardComponent
   ],
   imports: [
     BrowserModule,
