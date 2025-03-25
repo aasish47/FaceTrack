b728b1436adfbe2c487480eef915c5260a2f2f70
Akshaj Basur <akshajbasur@gmail.com>
2025-03-26 02:26:05 +0530

    Upadated user-dashboard

--- Frontend/src/app/Components/user-panel/user-dashboard/user-dashboard.component.ts
+++ Frontend/src/app/Components/user-panel/user-dashboard/user-dashboard.component.ts
@@ -48,10 +48,11 @@ export class UserDashboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    if(!localStorage.getItem('userLoggedIn')){
-      this.router.navigate(['/login']);
-    }
-    const userId = Number(sessionStorage.getItem('userId')); // Replace with dynamic user ID
+    // if(!localStorage.getItem('userLoggedIn')){
+    //   this.router.navigate(['/login']);
+    // }
+    //const userId = Number(sessionStorage.getItem('userId')); 
+    const userId = 1;
 
     // Fetch User Details
     this.userService.getUserDetails(userId).subscribe((data: User) => {
