cec1e4e8020575a98a69e871a63526d01aab4fc4
Ashton <ashutosh20.acharya@gmail.com>
2025-03-26 16:00:49 +0530

    Update settings.py

--- Backend/FaceRecognitionBackend/FaceRecognitionBackend/settings.py
+++ Backend/FaceRecognitionBackend/FaceRecognitionBackend/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
 #     }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'facetrack',
+        'NAME': 'testDB2',
         'USER': 'root',
         'PASSWORD': 'password',
         'PORT': '3306',
