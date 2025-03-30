b1040dc9b2dfa34a72f7a6ab2ddf8d66a4339ab6
AngelinaSwain <angelinaswn@gmail.com>
2025-03-30 16:19:21 +0530

    Fixed logic for user charts

--- Backend/FaceRecognitionBackend/FaceRecognitionBackend/settings.py
+++ Backend/FaceRecognitionBackend/FaceRecognitionBackend/settings.py
@@ -99,9 +99,9 @@ DATABASES = {
 #     }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'FaceTrack',
+        'NAME': 'testDB2',
         'USER': 'root',
-        'PASSWORD': 'Angelina@17',
+        'PASSWORD': 'password',
         'PORT': '3306',
         'HOST': '127.0.0.1'
 
